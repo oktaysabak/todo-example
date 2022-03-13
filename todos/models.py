cf23416af0aff00eb39190a4291869876801b598
oktay <oktay.sabak@kreatize.com>
2022-03-14 00:21:27 +0300

    Create todo model.

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    """Todo model class."""
+    detail = models.CharField(max_length=255, null=False)
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='todos', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """Object representaion method to show first 10 character of todo detail."""
+        return f'{self.detail[:10]}+{"..."}' 
